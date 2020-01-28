52c3dcf133d90d82f5111f1c0149b8e41de3712f
Philipp Meng <philipp.meng@gmail.com>
2020-01-28 13:41:52 +0100

    Medizintechnik Cluster Oberösterreich hinzugefügt

---
+++ bridges/MedTechClusterOOE.php
@@ -0,0 +1,31 @@
+<?php
+class VMwareSecurityBridge extends BridgeAbstract {
+
+	const MAINTAINER = 'izintu.at';
+	const NAME = 'Medizintechnik Cluster Oberösterreich ';
+	const URI = 'https://medizintechnik-cluster.at/news-presse';
+	const CACHE_TIMEOUT = 86400; // 24h
+	const DESCRIPTION = 'News aus dem Medizintechnik-Cluster Oberösterreich';
+	const WEBROOT = 'https://medizintechnik-cluster.at';
+
+	public function collectData(){
+		$html = getSimpleHTMLDOM(self::URI)
+		or returnServerError('Could not Request MedTechCluster.');
+
+		$html = defaultLinkTo($html, self::WEBROOT);
+
+		$item = array();
+		$articles = $html->find('div[class="col-md-8"]');
+
+		foreach ($articles as $element) {
+			$item['uri'] = $element->find('a', 0)->getAttribute('href');
+			$title = $element->find('a', 0)->innertext;
+			$item['title'] = $title;
+			$item['timestamp'] = strtotime($element->find('p[class="ecx-date"]', 0)->innertext);
+			$item['content'] = $element->find('p', 1)->innertext;
+			$item['uid'] = $title;
+
+			$this->items[] = $item;
+		}
+	}
+}
